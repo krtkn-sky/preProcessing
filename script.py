bc17cfa1460753b91e4c32aaa3ddf2a4b48d3b89
Aakash Karthikeyan <aakashkarthikeyan3@gmail.com>
2023-09-29 09:05:07 +0530

    Python file

--- script.py
+++ script.py
@@ -1,10 +1,21 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
+from sklearn.impute import SimpleImputer
+
 
 dataset = pd.read_csv("Data.csv")
-x = dataset.iloc[:,:-1].values
-print(x)
+X = dataset.iloc[:,:-1].values
+y = dataset.iloc[:,-1].values
 
-#print(dataset)
+imputer = SimpleImputer(missing_values=np.nan, strategy='mean')
+
+#fit method works only on numerical data
+
+imputer.fit(X[:,1:3])
+X[:,1:3] = imputer.transform(X[:,1:3])
+
+#print(X)
+
+print(dataset)
 
