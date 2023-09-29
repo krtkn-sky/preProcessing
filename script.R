571e72a24ec975ccee428d882a85085a28cb163e
Aakash Karthikeyan <aakashkarthikeyan3@gmail.com>
2023-09-29 15:53:49 +0530

    Changed

---
+++ script.R
@@ -0,0 +1,3 @@
+dataset = read.csv('Data.csv')
+dataset
+
--- script.py
+++ script.py
@@ -4,6 +4,7 @@ import pandas as pd
 from sklearn.impute import SimpleImputer
 from sklearn.compose import ColumnTransformer
 from sklearn.preprocessing import OneHotEncoder
+from sklearn.preprocessing import LabelEncoder
 
 dataset = pd.read_csv("Data.csv")
 X = dataset.iloc[:,:-1].values
@@ -15,9 +16,12 @@ imputer = SimpleImputer(missing_values=np.nan, strategy='mean')
 
 imputer.fit(X[:,1:3])
 X[:,1:3] = imputer.transform(X[:,1:3])
-print(X)
+
 ct = ColumnTransformer(transformers=[('encoder',OneHotEncoder(),[0])],remainder='passthrough')
 X = np.array(ct.fit_transform(X))
+
+le = LabelEncoder()
+y = le.fit_transform(y)
 
 #print(X)
 #print(dataset)
