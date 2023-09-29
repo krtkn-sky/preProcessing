33489a1619c60aee3fc8f6a0cf95a5112aaf3d40
Aakash Karthikeyan <aakashkarthikeyan3@gmail.com>
2023-09-29 16:54:25 +0530

    R

--- script.R
+++ script.R
@@ -1,3 +1,11 @@
 dataset = read.csv('Data.csv')
-View(dataset)
 
+dataset$Years = ifelse(is.na(dataset$Years),
+ave(dataset$Years,FUN=function(x) mean(x, na.rm=TRUE)),
+dataset$Years)
+
+dataset$Earnings = ifelse(is.na(dataset$Earnings),
+ave(dataset$Earnings,FUN=function(x) mean(x, na.rm=TRUE)),
+dataset$Earnings)
+
+#View(dataset)
